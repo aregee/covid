2c64ae42465eb8b97af993ea89d9e1508ab434e1
aregee <rahul.nbg@gmail.com>
2020-05-11 12:25:05 +0000

    Deploying to gh-pages from  @ 1082251e1b0838512fa7527132efd7d79c06eaab 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64427d4aae789569ab3ee83cb50190e5.js"
+  "/precache-manifest.391e0c0f8079cee4cad7ec3d45a6a924.js"
 );
 
 self.addEventListener('message', (event) => {
